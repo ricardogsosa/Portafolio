bfaf4db3cd86ccd9cfeb7998ce4ee0387a39b8a8
ricardogsosa <ricardogsosa0411@gmail.com>
2025-11-08 23:08:32 -0600

    Portafolio

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,40 @@
 import { defineConfig } from "vite";
-import path, {resolve} from "node:path";
-import * as glob from 'glob';
+import path from "node:path";
+import * as glob from "glob";
 
-import { ViteMinifyPlugin } from 'vite-plugin-minify';
-import HtmlCssPurgePlugin from 'vite-plugin-purgecss';
-import HandlebarsPlugin from 'vite-plugin-handlebars';
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+import HtmlCssPurgePlugin from "vite-plugin-purgecss";
+import HandlebarsPlugin from "vite-plugin-handlebars";
+import { getData } from "./src/data";
 
-import { getData } from './src/data';
+const { resolve } = path;
 
-function obtenerEntradas(){
-    return Object.fromEntries(
-        /*[
-            ["index", resolve(__dirname, "index.html")],
-            ["contactanos", resolve(__dirname, "contactanos.html")]
-        ]*/
-       glob.sync(
-        './**/*.html',
-        {
-            ignore: [
-                './dist/**',
-                './node_modules/**'
-            ]
-        }
-       ).map((file)=>{
-        return [
-            file.slice(0, file.length - path.extname(file).length),
-            resolve(__dirname, file)
-        ]
-       })
-    );
+function obtenerEntradas() {
+  const files = glob.sync("./*.html"); 
+  return Object.fromEntries(
+    files.map((file) => [
+      file.slice(0, file.length - path.extname(file).length),
+      resolve(dirname, file),
+    ])
+  );
 }
 
-export default defineConfig(
-    {
-        appType: 'mpa',
-        base: process.env.DEPLOY_BASE_URL ,
-        build: {
-            minify: true,
-            rollupOptions: {
-                input: obtenerEntradas()
-            }
-        },
-        plugins: [
-            HandlebarsPlugin({
-                partialDirectory: resolve(__dirname, 'src', 'partials'),
-                context: (page)=>{
-                    return getData(page);
-                }
-            }),
-            HtmlCssPurgePlugin(),
-            ViteMinifyPlugin()
-        ]
-    }
-);
+export default defineConfig({
+  appType: "mpa",
+  base: "/Portafolio/", 
+  build: {
+    outDir: "dist",
+    minify: true,
+    rollupOptions: {
+      input: obtenerEntradas(),
+    },
+  },
+  plugins: [
+    HandlebarsPlugin({
+      partialDirectory: resolve(dirname, "src", "partials"),
+      context: (pagePath) => getData(pagePath),
+    }),
+    HtmlCssPurgePlugin(),
+    ViteMinifyPlugin(),
+  ],
+});
